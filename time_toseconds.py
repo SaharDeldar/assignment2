2d099968a769acd8965832a3fd69e1be6aba7cf1
Sahar <100313500+SaharDeldar@users.noreply.github.com>
2022-07-13 18:43:38 +0430

    Add files via upload

---
+++ time_toseconds.py
@@ -0,0 +1,14 @@
+
+
+
+time=input("enter time 00:00:00 " )
+time=time.split(':')
+second=int(time[2])
+min=int(time[1])
+hour=int(time[0])
+if 0<=second<60 and 0<=min<60 and 0<=hour<24:
+    sum=hour*3600+min*60+second
+    print(sum,'seconds')
+
+else:
+    print("Enter the correct time :")
